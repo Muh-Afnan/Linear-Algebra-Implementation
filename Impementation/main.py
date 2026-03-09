b7d0694e750721cc24597382547eeca08e36acc6
Muh-Afnan <muhammadafnan108@gmail.com>
2026-03-09 16:09:00 +0500

    Refactor matrix multiplication and addition methods; enhance matrix chain multiplication logic

--- Impementation/main.py
+++ Impementation/main.py
@@ -1,3 +1,8 @@
+import array
+
+from streamlit import empty
+
+
 class MatrixValidation:
     def validate_addition(self,array):
         try:
@@ -44,33 +49,47 @@ class MatrixValidation:
             array.append(result)
         return array[0]
     
-    def matrix_multip(self, matrix1,matrix2):
-        item2_trans = list[zip(*matrix2)]
-        result = [[item1*item2 for item1,item2 in zip(matrix1,matrix2)] for row1,row2 in zip(matrix1,matrix2)]
-        return result  
+    def matrix_multiply(self, matrix1,matrix2):
+        item2_trans = list(zip(*matrix2))
+        result = []
+        for m1_row in matrix1:
+            row = []
+            for m2_row in item2_trans:
+                sum = 0
+                for item1,item2 in zip(m1_row,m2_row):
+                    sum += item1*item2
+                row.append(sum)
+            result.append(row)
+        return result
       
-    def matrix_multiplication(self,matrix1:list[list[int|float]], matrix2:list[list[int|float]],*othermatrix):
-        array = [matrix,matrix2,*othermatrix]
+    def matrix_chain_multiply(self,matrix1:list[list[int|float]], matrix2:list[list[int|float]],*othermatrix):
+        array = [matrix1,matrix2,*othermatrix]
         self.validate_addition(array)
-        while len(array) >1:
-            matrix1 = array.pop()
-            matrix2 = array.pop()
-            result = self.matrix_add(matrix1,matrix2)
-            array.append(result)
-        return array[0]
+        result = array[0]
+        print(f"array: {array}")
+        print(f"len: {len(array)}")
+        for matrix in array[1:]:
+            result = self.matrix_multiply(result, matrix)
+        return result
+
+    def matrix_transpose(self,matrix):
+        return list(zip(*matrix))
 
 
+# matrix = [[1,2],[112,8],[2,3]]
+# matrix2 = [[1,2,3],[112,56,8],[1,2,3]]
+# matrix3 = [[1,2,3],[112,56,8],[1,2,3]]
+# matrix4 = [[1,2],[2,2]]
 
-matrix = [[1,2],[112,8],[2,3]]
-matrix2 = [[1,2,3],[112,56,8],[1,2,3]]
-matrix3 = [[1,2,3],[112,56,8],[1,2,3]]
-matrix4 = [[1,2],[2,2]]
-mat_op = MatrixValidation()
-# print(mat_op.validate_addition(matrix,matrix2,matrix3))
-# print(mat_op.scalar_multiplication(matrix=matrix,number=6))
-# print(mat_op.scalar_addition(matrix=matrix,number=6))
-# print(mat_op.matrix_addition(matrix,matrix2))
-print(mat_op.matrix_multiplication(matrix,matrix4))
+# m1 = [[1,2,3],[4,5,6]]
+# m2 = [[1,2],[3,4],[5,6]]
+# m3 = [[1,2],[3,4]]
+# mat_op = MatrixValidation()
+# # print(mat_op.validate_addition(matrix,matrix2,matrix3))
+# # print(mat_op.scalar_multiplication(matrix=matrix,number=6))
+# # print(mat_op.scalar_addition(matrix=matrix,number=6))
+# # print(mat_op.matrix_addition(matrix,matrix2))
+# print(mat_op.matrix_chain_multiply(m1,m2,m3))
 
 
 
